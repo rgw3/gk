32af834878e2f33fc7bc1492e34cccb1781d0d72
lnu <rgw@alumni.utexas.net>
2026-08-20 15:00:09 -0500

    Initial Commit

---
+++ GoalKick/GoalKickApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoalKickApp.swift
+//  GoalKick
+//
+//  Created by Rocket Williams on 8/20/26.
+//
+
+import SwiftUI
+
+@main
+struct GoalKickApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GoalKickTests/GoalKickTests.swift
@@ -0,0 +1,19 @@
+//
+//  GoalKickTests.swift
+//  GoalKickTests
+//
+//  Created by Rocket Williams on 8/20/26.
+//
+
+import Testing
+@testable import GoalKick
+
+struct GoalKickTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
